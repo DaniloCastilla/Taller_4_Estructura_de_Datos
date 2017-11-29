6d50049978ba3f77d407d38eab1ceeb1e3d991d6
Eddycas <32119229+Eddycas@users.noreply.github.com>
2017-11-29 02:54:23 -0500

    Update Binay_Search.py

--- Binay_Search.py
+++ Binay_Search.py
@@ -3,62 +3,67 @@ Created on 28 nov. 2017
 
 @author: Danicas99
 '''
-from random import randint #import library random
-from builtins import str #import library String
+from random import randint
+from builtins import str
 
 
-def Quick_sort(V): #funtion quick sort, parameter: Array
-    pivote = len(V)//2 #Mark pivote (index)
-    menor, igual, mayor = [],[],[] #create three array
+def Quick_sort(V): #Quick Sort function
+    pivote = len(V)//2
+    menor, igual, mayor = [],[],[]
     
-    for i in range(len(V)):#cycle for since 0 to tamain of the array
-        if(V[i] < V[pivote]): menor.append(V[i]) #conditional (if array v in the possition i is less to array v in the possition pivote, add to menor this value
-        elif(V[i]> V[pivote]): mayor.append(V[i])#conditional (if array v in the possition i is gigher to array v in the possition pivote, add to menor this value
-        else: igual.append(V[i])#end conditional #conditional (if array v in the possition i is gigher to array v in the possition pivote, add to menor this value
+    for i in range(len(V)):
+        if(V[i] < V[pivote]): menor.append(V[i])
+        elif(V[i]> V[pivote]): mayor.append(V[i])
+        else: igual.append(V[i])
         
-    if len(menor)>1: #if the size is higher to 1, join the funtion and will arrange 
-        menor = Quick_sort(menor)#order less
-    if len(mayor)>1: #if the size is higher to 1, join the funtion and will arrange 
-        mayor = Quick_sort(mayor) #order higher
-    return menor + igual + mayor #return the three arrays in order
-
-def B_S(V , X):#funtion binary search, parameter: Array and Value to search
+    if len(menor)>1: 
+        menor = Quick_sort(menor)
+    if len(mayor)>1: 
+        mayor = Quick_sort(mayor)
+    return menor + igual + mayor
+    #end funtion
+def B_S(V , X): #Binary search Function
     
-    Inicio = 0 #begin the possition 0
-    Final = (len(V)-1) #end the final possition
-    Encontro = False #discriminator
+    Inicio = 0
+    Final = (len(V)-1)
+    T = False
     
-    while(not(Encontro) and Inicio <= Final):#while the discriminator is False, do
-        mitad = int((Inicio + Final)/2) #assignate a valuer integer to mitad
-        if(X == V[mitad]): #conditional where he asks if it's equal to mitad
-            Encontro = True #if is true, encontro will be true and end the cycle
-            Num = mitad #save the index
-        elif(X < (V[mitad])): #conditional where he asks if it's less to mitad
+    while(not(T) and Inicio <= Final):
+        mitad = int((Inicio + Final)/2)
+        if(X == V[mitad]):
+            T = True
+            Num = mitad
+        elif X < V[mitad]:
             Final = mitad - 1
             
-        else:#conditional where he asks if it's higher to mitad
+        else:
             Inicio = mitad + 1
     print()
     
-    if(Encontro):#if encontro is true; will return a messagge with its index
+    if(T):
         return("El Numero se encuentra y se localiza en la posicion " + str(Num))
-    else:#if encontro is false; will return a messagge with its index
+    else:
         return("El dato no se encuentra")
     print()
-
-def main():
+    #end Binary Search Funtion
+def main(): #Class main
     
-    Tamain = (input("Escriba la Sucesion de numeros separados por comas \n"))#write number separte with commas
-    print(Tamain)#to print
+    Tamain = (input("Escriba la Sucesion de numeros separados por comas \n"))
     Tamain_2 = Tamain.split(",")
-    print(Tamain_2)#divide in arrays
-    Tamain_3 = Quick_sort(Tamain_2)#order the new array
-    print(Tamain_3)#to print array
+    print()
+    Tamain_3 = Tamain.split(",")
     
-    Valor = (input("Ingrese un numero a buscar \n"))#value to search
+    for i in range (0,len(Tamain_2)):
+        Tamain_3[i] = int(Tamain_2[i])
+    print()
     
-    Binary = B_S(Tamain_3, Valor)#call funtion search
-    print(Binary)#to print messagge and index
+    Tamain_4 = (Quick_sort(Tamain_3))
+    print("El Arreglo ordenado es " + str(Tamain_4))
+    
+    Valor = int(input("Ingrese un numero a buscar \n"))
+    
+    Binary = B_S(Tamain_4, Valor)
+    print(Binary)
        
        
-main()
+main()#end class main

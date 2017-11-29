52f1c0ce59d45e2a001b6c40f13728ac21c2e49b
Eddycas <32119229+Eddycas@users.noreply.github.com>
2017-11-29 04:10:40 -0500

    Update Interpolation_Sort.java

--- Interpolation_Sort.java
+++ Interpolation_Sort.java
@@ -1,23 +1,61 @@
-/*
-*
-*TALLER 4 - Estructura de Datos-
-*	@author Danicas99
-*	@date 25-11-2017
-*	@version 2
-*
-*/
-
+/**
+ * @author Danicas99
+ * @date 28/11/2017
+ * @version 1.0
+ */
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
+public class Interpolation_Search {//Main Class
+	 public static void imprimeArray(int[] array){
+	        for (int i = 0; i < array.length; i++) {
+	            System.out.print("[" + array[i] + "]");
+	        }
+	 }
+	 public static void ordenacionMergeSort(int Array[])
+	 {//Method that assigns new arrays to divide
+	if(Array.length<=1) return;
+		int mitad= Array.length/2;
+		int menor[]=Arrays.copyOfRange(Array, 0, mitad);
+		int mayor[]=Arrays.copyOfRange(Array, mitad, Array.length);
+			ordenacionMergeSort(menor);
+			ordenacionMergeSort(mayor);       
+			combinarVector(Array, menor, mayor);
+	 }//end method
 
-public class Interpolation_Search { //create class
+	 public static void combinarVector(int v[], int izq[],int der[])
+	 {//Method that starts to make combinations of left and right
+       int i=0;
+       int j=0;
+       for(int k=0;k<v.length;k++){
+               if(i>=izq.length){
+                       v[k]=der[j];
+                       j++;
+                       continue;
+               }
+               if(j>=der.length){
+                       v[k]=izq[i];
+                       i++;
+                       continue;
+               }
+               if(izq[i]<der[j]){
+                       v[k]=izq[i];
+                       i++;
+               }else{
+                       v[k]=der[j];
+                       j++;
+               }
+       }
+	 }//end method
 	
- static int BusquedaInterpolacion(int x, int []arr) { //parameter, array and X with value to search
+ static int BusquedaInterpolacion(int x, int []arr) 
+ {//Method that verifies and searches for the value entered
+	 
 	 int lowerBound = 0;
 	 int higherBound = (arr.length - 1);
 	 while (lowerBound <= higherBound && x >= arr[lowerBound] && x <= arr[higherBound]){
@@ -32,11 +70,9 @@ public class Interpolation_Search { //create class
 	    	  higherBound = middle - 1;
 	        }
 	        return -1;
-	    }
+	    }//end method
 
-
-
- public static void main(String[] args) throws IOException { //main method
+ public static void main(String[] args) throws IOException {//Main method
 	BufferedReader br = new BufferedReader (new InputStreamReader (System.in));	
 	BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));		
 	
@@ -57,6 +93,10 @@ public class Interpolation_Search { //create class
 		  System.out.print("["+array[i]+"]");
 		}
 	
+	System.out.println("\n"+"Merge Sort");
+	ordenacionMergeSort(array);
+	imprimeArray(array);
+	
 	bw.write("\nEscriba el numero que desea buscar");
 	bw.flush();	
 	int buscar = Integer.parseInt(br.readLine());
@@ -70,7 +110,6 @@ public class Interpolation_Search { //create class
 	
 	else
 	System.out.println("el numero no se encuentra dentro del arreglo");
-    
-				
-	  }
-}
+    	
+	  }//end Method
+}//end Class
